bad51202e546a980658b5d71c8de751b762bf947
Mário Prada <mariosilvaprada@gmail.com>
2020-07-28 22:51:16 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,36 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Button from "../components/button"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Button from '../components/button';
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = 'Gatsby Starter Personal Website';
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
+        <SEO title="Home" keywords={[ `blog`, `gatsby`, `javascript`, `react` ]} />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people{" "}
+          Hey, I'm Mário Prada.{' '}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          Welcome to my internet corner where I write about topics related to Javascript. Some of
+          them I&apos;m comfortable with, and others not so much but that&apos;s why I want to
+          confront them. Feel yourself at home.
         </p>
-        <p>Now go build something great!</p>
+
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
       </Layout>
-    )
+    );
   }
 }
 
-export default IndexPage
+export default IndexPage;
