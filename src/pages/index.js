1c2a2b38b6fbd7769b543b5fb2c34da6499049d4
Mário Prada <mariosilvaprada@gmail.com>
2020-07-29 14:35:54 +0100

    updating

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,30 +5,37 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Button from '../components/button';
 
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../utils/theme';
+
 class IndexPage extends React.Component {
   render() {
     const siteTitle = 'Gatsby Starter Personal Website';
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="Home" keywords={[ `blog`, `gatsby`, `javascript`, `react` ]} />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey, I'm Mário Prada.{' '}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>
-          Welcome to my internet corner where I write about topics related to Javascript. Some of
-          them I&apos;m comfortable with, and others not so much but that&apos;s why I want to
-          confront them. Feel yourself at home.
-        </p>
+    const Header = styled.h1`color: ${({ theme }) => theme.dark.header};`;
 
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
-      </Layout>
+    return (
+      <ThemeProvider theme={theme}>
+        <Layout location={this.props.location} title={siteTitle}>
+          <SEO title="Home" keywords={[ `blog`, `gatsby`, `javascript`, `react` ]} />
+          <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+          <Header>
+            Hey, I'm Mário Prada.{' '}
+            <span role="img" aria-label="wave emoji">
+              👋
+            </span>
+          </Header>
+          <p>
+            Welcome to my internet corner where I write about topics related to Javascript. Some of
+            them I&apos;m comfortable with, and others not so much but that&apos;s why I want to
+            confront them. Feel yourself at home.
+          </p>
+
+          <Link to="/blog/">
+            <Button marginTop="35px">Go to Blog</Button>
+          </Link>
+        </Layout>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/utils/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+  fontSize: {
+    small: '.8rem',
+    medium: '1rem',
+    big: '2rem',
+    title: '2.5rem'
+  },
+  size: {
+    maxWidth: '42.5rem'
+  },
+  dark: {
+    color: 'white',
+    background: '#222831',
+    header: '#e43f5a'
+  },
+  light: {
+    color: '#222831',
+    background: 'white',
+    header: '#dd2c00'
+  }
+};
+
+export default theme;
