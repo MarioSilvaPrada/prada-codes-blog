2f18ed9673be8c97e165d707c9a922eb3868c770
Mário Prada <mariosilvaprada@gmail.com>
2020-07-30 11:47:02 +0100

    changing theme location

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,3 @@
 // custom typefaces
 import 'typeface-montserrat';
 import 'typeface-merriweather';
-
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './src/utils/theme';
-
-export const wrapRootElement = ({ element }) => {
-  return (
-    <ThemeProvider theme={theme}>
-      {element}
-    </ThemeProvider>
-  );
-};
--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -3,24 +3,27 @@ import Header from '../Header/Header';
 import { rhythm } from '../../utils/typography';
 import { Container } from './layout.styled';
 import Footer from '../Footer';
+import theme from '../../utils/theme';
 
 const Layout = ({ children }) => {
   const [ isLightMode, setIsLightMode ] = useState(false);
   return (
-    <Container isLightMode={isLightMode}>
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
-        }}
-      >
-        <Header isLightMode={isLightMode} setIsLightMode={setIsLightMode} />
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container isLightMode={isLightMode}>
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
+          }}
+        >
+          <Header isLightMode={isLightMode} setIsLightMode={setIsLightMode} />
+          <main>{children}</main>
+          <Footer />
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 };
 
